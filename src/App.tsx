58b93c0d043f5095d721e822cf0e31120d638cfa
Medlwill <yuraiinaumov@gmail.com>
2025-05-18 18:18:25 +0200

    create 2 task arrays

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import './App.css'
 import {Todolist} from "./Components/Todolist.tsx";
 
 function App() {
+
+    const truck1 = "What to learn-1"
+    const truck2 = "What to learn-2"
+
+    const tasks1 = [
+        { id: 1, title: 'HTML&CSS', isDone: true },
+        { id: 2, title: 'JS', isDone: true },
+        { id: 3, title: 'ReactJS', isDone: false },
+    ]
+
+    const tasks2 = [
+        { id: 1, title: 'Hello world', isDone: true },
+        { id: 2, title: 'I am Happy', isDone: false },
+        { id: 3, title: 'Yo', isDone: false },
+
     return (
         <div className="app">
-            <Todolist truck={"What to learn-1"}/>
-            <Todolist truck={"What to learn-2"}/>
-            <Todolist/>
+            <Todolist truck={truck1} task={tasks1}/>
+            <Todolist truck={truck2} task={tasks2}/>
         </div>
     )
 }
