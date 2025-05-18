e2b78d0c9a8bef3303fca4d9a18d1fa0667b5cd9
Medlwill <yuraiinaumov@gmail.com>
2025-05-18 18:21:08 +0200

    creat type of two arrays

---
+++ src/Components/Todolist.tsx
@@ -0,0 +1,39 @@
+type TodolistProps = {
+    truck: string
+    task: TaskProps[]
+}
+
+type TaskProps = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const Todolist = ({truck}: TodolistProps) => {
+
+    return (
+            <div>
+                <h3>{truck}</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li>
+                        <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
+                    </li>
+                    <li>
+                        <input type="checkbox" checked={true}/> <span>JS</span>
+                    </li>
+                    <li>
+                        <input type="checkbox" checked={false}/> <span>React</span>
+                    </li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+    );
+};
