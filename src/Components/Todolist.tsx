8615035b280fd19bd236d04c2320ce8a7566a219
Medlwill <yuraiinaumov@gmail.com>
2025-09-04 21:46:51 +0200

    writing local useState and writing functions in the component for comfort coding

--- src/Components/Todolist.tsx
+++ src/Components/Todolist.tsx
@@ -1,6 +1,7 @@
 // import {Button} from './Button'
 import {FilterValueType, TaskType} from "../App.tsx";
-import {useRef} from "react";
+import {ChangeEvent, KeyboardEvent, useState} from "react";
+
 // import {useState} from "react";
 
 
@@ -9,44 +10,68 @@ export type Props = {
     changeFilter: (val: FilterValueType) => void
     tasks: TaskType[]
     title: string
-    addTask: (value: string) => void
+    addTask: (newTitle: string) => void
 }
 
 export const Todolist = ({title, tasks, removeTasks,changeFilter, addTask}: Props) => {
-        const inputRef = useRef<HTMLInputElement>(null);
+
+        const [newTitle, setNewTitle] = useState('')
+
+        // const changeFilterAllHandler = () =>  changeFilter('All')
+        // const changeFilterActiveHandler = () =>  changeFilter('Active')
+        // const changeFilterCompletedHandler = () =>  changeFilter('Completed')
+
+
+        const changeFilterHandler = (value: FilterValueType) =>  changeFilter(value)
+        const addTaskHandler = () =>{
+            addTask(newTitle)
+            setNewTitle('')
+        }
+        const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement> ) => {
+                if(e.key === 'Enter'){
+                    addTaskHandler()
+                }
+        }
+        const onChangeHandler = (e: ChangeEvent<HTMLInputElement> ) => {
+            setNewTitle(e.currentTarget.value)
+        }
+        const mappedTasks = tasks.map(task => {
+            const removeTaskHandler = () => {
+                removeTasks(task.id)
+            }
+            return (
+                <li key={task.id}>
+                    <button onClick={removeTaskHandler}>x</button>
+                    <input type="checkbox" checked={task.isDone}/>
+                    <span>{task.title}</span>
+                </li>
+            )
+        })
+
 
         return (
             <div>
                 <h3>{title}</h3>
                 <div>
-                    <input ref={inputRef}/>
+                    <input value={newTitle}
+                           onChange={onChangeHandler}
+                            onKeyPress={onKeyPressHandler}
+                    />
                     {/*<Button title={'+'}/>*/}
-                    <button onClick={()=> {
-                        if (inputRef.current) {
-                            addTask(inputRef.current.value);
-                            inputRef.current.value = "";
-                        }
-                    }}>+</button>
+                    <button onClick={addTaskHandler}>+</button>
                 </div>
                 {tasks.length === 0 ? (
                     <p>Тасок нет</p>
                 ) : (
                     <ul>
-                        {tasks.map(task => {
-                            return (
-                                <li key={task.id}>
-                                    <button onClick={() => removeTasks(task.id)}>x</button>
-                                    <input type="checkbox" checked={task.isDone}/>
-                                    <span>{task.title}</span>
-                                </li>
-                            )
-                        })}
+                        {mappedTasks}
                     </ul>
                 )}
                 <div>
-                    <button onClick={() => changeFilter('All')}>All</button>
-                    <button onClick={() => changeFilter('Active')}>Active</button>
-                    <button onClick={() => changeFilter('Completed')}>Completed</button>
+                    <button onClick={() => changeFilterHandler('All')}>All</button>
+                    <button onClick={() => changeFilterHandler('Active')}>Active</button>
+                    <button onClick={() => changeFilterHandler('Completed')}>Completed</button>
+                    {/*<button onClick={changeFilterCompletedHandler}>Completed</button>*/}
                 </div>
             </div>
         )
